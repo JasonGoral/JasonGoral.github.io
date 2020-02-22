ff047c1a82bf5d2b37f7ed10f8578cc23c68c0b6
JasonGoral <JasonGorals@gmail.com>
2020-02-21 20:17:01 -0500

    New menu button and js added to index and mainpagecss, also started updateding the pics to 8000x800

---
+++ js/navButton.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$("#navbar").hide();
+
+ $("div.openNav").click(function(){
+	$("#navbar").show();
+	});
+
+ $("div#closeBut").click(function(){
+	$("#navbar").hide();
+	});
+
+
+});
